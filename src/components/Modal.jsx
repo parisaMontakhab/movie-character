ce403a3d3c6254898f8a2dbba01c593c960f2190
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-23 13:48:34 +0200

    feat: add div modal-header classname in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,6 +4,9 @@ export default function Modal() {
   return (
     <div className="backdrop">
         <div className="modal">
+            <div className="modal__header">
+
+            </div>
 
         </div>
 
