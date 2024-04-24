b6506daa77665608f83483fc9d14dcd2e9bba4a6
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-24 13:36:48 +0200

    feat: add setisopen in navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import {HeartIcon} from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function NavBar({numOfResult,query,setQuery,favourites}) {
+  const[isOpen,setIsOpen] = useState(false);
   return (
    <nav className="navbar">
     <div className="navbar__logo ">LOGO</div>
     <input type="text" className="text-field" placeholder="search..." value={query} onChange={(e)=> setQuery(e.target.value)}/>
     <div className="navbar__result">"found {numOfResult} characters"</div>
-    <button className="heart">
+    <button className="heart" onClick={()}>
+       
         <HeartIcon className="icon"/>
         <span className="badge">{favourites.length}</span>
     </button>
