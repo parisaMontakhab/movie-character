3783fc4c3263e78f4b2f51b172616236b693ae86
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-24 12:23:52 +0200

    feat: add onClick at button and div backdrop at Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,11 +3,11 @@ import { XCircleIcon } from "@heroicons/react/16/solid";
 
 export default function Modal({title,children}) {
   return (
-    <div className="backdrop">
+    <div className="backdrop" onClick={()=>onOpen(false)}>
         <div className="modal">
             <div className="modal__header">
                 <h2 className="title">{title}</h2>
-                <button>
+                <button onClick={()=>onOpen(false)}>
                     <XCircleIcon className="icon close"/>
                 </button>
 
