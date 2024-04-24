76ea5583c9217ea5ecb36f75eed4f06dbacf6709
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-24 14:08:55 +0200

    refactor: extract button from characterItem component and props to  character as children for having daynamic characterItem sectetion icon

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,35 +1,53 @@
-import { EyeIcon, EyeSlashIcon} from '@heroicons/react/24/outline';
-import Loader from './Loader';
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+import Loader from "./Loader";
 
-
-
-export default function CharacterList({characters,isLoading,onSelectedCharacter,selectedId}) {
- 
+export default function CharacterList({
+  characters,
+  isLoading,
+  onSelectedCharacter,
+  selectedId,
+}) {
   return (
     <div className="characters-list ">
-      { isLoading? (<Loader/>):
-     ( characters.map((item)=>
-      <CharacterItem key={item.id} item={item} onSelectedCharacter={onSelectedCharacter} selectedId={selectedId}/>
-    ))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        characters.map((item) => (
+          <CharacterItem
+            key={item.id}
+            item={item}
+            onSelectedCharacter={onSelectedCharacter}
+            selectedId={selectedId}
+          >
+            <button
+              className="icon red"
+              onClick={() => onSelectedCharacter(item.id)}
+            >
+              {selectedId === item.id ? <EyeSlashIcon /> : <EyeIcon />}
+            </button>
+          </CharacterItem>
+        ))
+      )}
     </div>
-  )
+  );
 }
 
- export function CharacterItem({item,onSelectedCharacter,selectedId}){
-    return(
-        <div className="list__item">
-            <img src={item.image} alt={item.name}/>
-            <h3 className="name">{item.name}</h3>
-            <div className="list-item__info info">
-                <span className={`status ${item.status==="Dead" ? "red" : " "}`}></span>
-                <span> {item.status}</span>
-                <span> - {item.species}</span>
-
-            </div>
-            <button className="icon red" onClick={()=>onSelectedCharacter(item.id)}>
-             {selectedId === item.id ? <EyeSlashIcon/>:<EyeIcon/>}
-            </button>
-
-        </div>
-    )
+export function CharacterItem({
+  item,
+  children,
+}) {
+  return (
+    <div className="list__item">
+      <img src={item.image} alt={item.name} />
+      <h3 className="name">{item.name}</h3>
+      <div className="list-item__info info">
+        <span
+          className={`status ${item.status === "Dead" ? "red" : " "}`}
+        ></span>
+        <span> {item.status}</span>
+        <span> - {item.species}</span>
+      </div>
+      {children}
+    </div>
+  );
 }
