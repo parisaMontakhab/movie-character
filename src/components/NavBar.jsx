33b78d134f3d68f69ff98f17dba85f166e883f8c
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-24 14:01:22 +0200

    refactor: ctr + s for showing better code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,34 @@
-import {HeartIcon} from "@heroicons/react/24/outline";
+import { HeartIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
-import  Modal  from "./Modal";
+import Modal from "./Modal";
 import { CharacterItem } from "./CharacterList";
 
-export default function NavBar({numOfResult,query,setQuery,favourites}) {
-  const[isOpen,setIsOpen] = useState(false);
+export default function NavBar({ numOfResult, query, setQuery, favourites }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-   <nav className="navbar">
-    <div className="navbar__logo ">LOGO</div>
-    <input type="text" className="text-field" placeholder="search..." value={query} onChange={(e)=> setQuery(e.target.value)}/>
-    <div className="navbar__result">"found {numOfResult} characters"</div>
-    <Modal onOpen={setIsOpen} open={isOpen} title="List of favourites">
-      {favourites.map((item)=>(
-      <CharacterItem item={item} onSelectedCharacter={()=>{}} selectedId="1"/>))}
-    </Modal>
-    <button className="heart" onClick={()=>setIsOpen((is)=>!is)}>
-        <HeartIcon className="icon"/>
+    <nav className="navbar">
+      <div className="navbar__logo ">LOGO</div>
+      <input
+        type="text"
+        className="text-field"
+        placeholder="search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div className="navbar__result">"found {numOfResult} characters"</div>
+      <Modal onOpen={setIsOpen} open={isOpen} title="List of favourites">
+        {favourites.map((item) => (
+          <CharacterItem
+            item={item}
+            onSelectedCharacter={() => {}}
+            selectedId="1"
+          />
+        ))}
+      </Modal>
+      <button className="heart" onClick={() => setIsOpen((is) => !is)}>
+        <HeartIcon className="icon" />
         <span className="badge">{favourites.length}</span>
-    </button>
-
-   </nav>
-  )
+      </button>
+    </nav>
+  );
 }
