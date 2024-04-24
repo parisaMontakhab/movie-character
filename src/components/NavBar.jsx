db539ff430fb8f4be28c29d76cd7df8913d73fa9
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-24 13:44:37 +0200

    feat: import Modal in Navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import {HeartIcon} from "@heroicons/react/24/outline";
 import { useState } from "react";
-
+import  Modal  from "./Modal";
 export default function NavBar({numOfResult,query,setQuery,favourites}) {
   const[isOpen,setIsOpen] = useState(false);
   return (
@@ -8,8 +8,8 @@ export default function NavBar({numOfResult,query,setQuery,favourites}) {
     <div className="navbar__logo ">LOGO</div>
     <input type="text" className="text-field" placeholder="search..." value={query} onChange={(e)=> setQuery(e.target.value)}/>
     <div className="navbar__result">"found {numOfResult} characters"</div>
+    <Modal onOpen={setIsOpen} open={isOpen} title="List of favourites"/>
     <button className="heart" onClick={()=>setIsOpen((is)=>!is)}>
-       
         <HeartIcon className="icon"/>
         <span className="badge">{favourites.length}</span>
     </button>
