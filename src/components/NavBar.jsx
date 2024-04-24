0a411e11aef05b66b61df78a3606e9173782a5cd
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-24 13:37:41 +0200

    feat: add onCklick on button at navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar({numOfResult,query,setQuery,favourites}) {
     <div className="navbar__logo ">LOGO</div>
     <input type="text" className="text-field" placeholder="search..." value={query} onChange={(e)=> setQuery(e.target.value)}/>
     <div className="navbar__result">"found {numOfResult} characters"</div>
-    <button className="heart" onClick={()}>
+    <button className="heart" onClick={()=>setIsOpen((is)=>!is)}>
        
         <HeartIcon className="icon"/>
         <span className="badge">{favourites.length}</span>
