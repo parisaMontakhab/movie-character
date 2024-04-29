390e2d30ed1fdf9cd15a0f4b0aef6c4ead30fb59
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-29 11:47:43 +0200

    refactor: changed the place of useeffect about fetch data and replace them to in useCharacter custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -13,35 +13,6 @@ function App() {
   const [selectedId, setSelectedId] = useState(null);
   const [favourites, setFavourites] = useState(()=>JSON.parse(localStorage.getItem("FAVOURITES"))||[]);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    async function fetchinApi() {
-     
-      try {
-        setIsLoading(true);
-        const { data } = await axios.get(
-          `https://rickandmortyapi.com/api/character?name=${query}`,{signal}
-        );
-
-        setCharacters(data.results.slice(0, 5));
-      } catch (err) {
-        if (!axios.isCancel()) {
-          toast.error(err.response.data.error);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    if (query.length < 3) {
-      setCharacters([]);
-      return;
-    }
-    fetchinApi();
-    return () => {
-      controller.abort();
-    };
-  }, [query]);
 
   useEffect(()=>{
     localStorage.setItem("FAVOURITES",JSON.stringify(favourites))
--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -1,3 +1,32 @@
 export function useCharacters (){
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        async function fetchinApi() {
+         
+          try {
+            setIsLoading(true);
+            const { data } = await axios.get(
+              `https://rickandmortyapi.com/api/character?name=${query}`,{signal}
+            );
+    
+            setCharacters(data.results.slice(0, 5));
+          } catch (err) {
+            if (!axios.isCancel()) {
+              toast.error(err.response.data.error);
+            }
+          } finally {
+            setIsLoading(false);
+          }
+        }
+        if (query.length < 3) {
+          setCharacters([]);
+          return;
+        }
+        fetchinApi();
+        return () => {
+          controller.abort();
+        };
+      }, [query]);
     
 }
